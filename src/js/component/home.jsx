fd29449796b94d364dd6c501aaba37c1c6f5eab7
Roberto Lopez J <roberto@lightsaint.com>
2022-06-30 03:51:56 +0000

    [Made Fetch with ToDo List]
    - Added fetch functionalities to the Todo List
    - If there is an empty "enter", it will also add
    a "take a break" Line.
    - Added Delete All Tasks Button once a first Task is added.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,81 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
+  const apiURL = "http://assets.breatheco.de/apis/fake/todos/user/etolopez";
   const [task, setTask] = useState([]);
   const [isShown, setIsShown] = useState(-1);
+  //  const [user, setUser] = useState(true)
 
+  useEffect(() => {
+    fetch(apiURL)
+      .then((result) => {
+        if (result.status == 200) {
+          return result.json();
+        }
+      })
+      .then(setTask(task))
+      .catch((err) => err);
+  }, []);
+
+  const createUser = function () {
+    fetch(apiURL, {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: { "Content-type": "application/json" },
+    });
+  };
+
+  const updateTask = function (task) {
+    console.log(task);
+    fetch(apiURL, {
+      method: "PUT",
+      body: JSON.stringify(task),
+      headers: { "Content-type": "application/json" },
+    })
+      .then((result) => result.json())
+      .then((data) => console.log("resultado metodo PUT: ", data))
+      .catch((err) => err);
+  };
+
+  const deleteUser = function (task) {
+    fetch(apiURL, {
+      method: "DELETE",
+      body: JSON.stringify(task),
+      headers: { "Content-type": "application/json" },
+    })
+      .then((result) => result.json())
+      .then((data) => console.log("resultado metodo DELETE: ", data))
+      .catch((err) => err);
+  };
 
   const handleDeleteTask = function (index) {
-    setTask(
-      task.filter((e, currentIndex) => {
-        return currentIndex !== index;
-      })
-    );
+    const filter = task.filter((item, currentIndex) => {
+      return currentIndex !== index;
+    });
+    setTask(filter);
+    updateTask(filter);
+  };
+
+  const handleCompletedTask = (index) => {
+    let copyTask = [...task];
+    copyTask[index].done = true;
+    setTask(copyTask);
+    updateTask(copyTask);
+  };
+
+  const handleDeleteAllTasks = () => {
+    setTask([]);
   };
 
   let style1 = {
     height: "15px",
     backgroundColor: "white",
     zIndex: -1,
-}
+  };
+
+
 
   return (
     <div className="text-center container m-auto">
@@ -33,56 +89,103 @@ const Home = () => {
                 type="text"
                 size={30}
                 placeholder="What else should we add?"
-                onKeyDown={(e) => {
-                  if (e.key == "Enter") {
-                    setTask([...task, e.target.value]);
+                onKeyDown={(item) => {
+                  if (item.key == "Enter") {
+                    setTask([
+                      ...task,
+                      { label: item.target.value, done: false },
+                    ]);
                   }
                 }}
               ></input>
             </div>
-            {task.map((e, index) => {
-              return (
-                <div
-                  className="row mt-1 p-0 border-top"
-                  onMouseEnter={() => setIsShown(index)}
-                  onMouseLeave={() => setIsShown(-1)}
-                >
-                  <div className="col-11 mt-3 pt-2 pb-2" key={index}>
-                    <div className="col" id="textList">
-                      {e}
+            {task.map((item, index) => {
+              if (item.label == ""){
+                return (
+                  <div
+                    key={index}
+                    className="row mt-1 p-0 border-top"
+                    onMouseEnter={() => setIsShown(index)}
+                    onMouseLeave={() => setIsShown(-1)}
+                  >
+                    <div className="col-1"></div>
+                    <div className="col-9 mt-3 mb-3 pt-2 pb-2">
+                      <div className={`col ${item.done ? "lineClass" : "" }`} id="textList1" >
+                        Take a break
+                      </div>
                     </div>
+                    {isShown == index ? (
+                      <div className="col-2 pt-4" key={index}>
+                        <span id="check" onClick={() => handleCompletedTask(index)}>✓</span>
+                        <span id="del" onClick={() => handleDeleteTask(index)}>X</span>
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
-                  {isShown == index ? (
-                    <div className="col-1 pt-4">
-                      <span onClick={() => handleDeleteTask(index)}>X</span>
+                );
+              } else {
+                return (
+                  <div
+                    key={index}
+                    className="row mt-1 p-0 border-top"
+                    onMouseEnter={() => setIsShown(index)}
+                    onMouseLeave={() => setIsShown(-1)}
+                  >
+                    <div className="col-1"></div>
+                    <div className="col-9 mt-3 mb-3 pt-2 pb-2">
+                      <div className={`col ${item.done ? "lineClass" : "" }`} id="textList" >
+                        {item.label}
+                      </div>
                     </div>
-                  ) : (
-                    ""
-                  )}
-                </div>
-              );
-            })}
+                    {isShown == index ? (
+                      <div className="col-2 pt-4" key={index}>
+                        <span id="check" onClick={() => handleCompletedTask(index)}>✓</span>
+                        <span id="del" onClick={() => handleDeleteTask(index)}>X</span>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                );
+              }
+              }
+             )}
           </div>
           {task.length === 0 ? (
             <div className="row border-top">
-            <div className="col-8 mt-2">
-              <p> No Items on the list</p>
+              <div className="col-8 mt-2">
+                <p> No Items on the list</p>
+              </div>
+              <div className="col-4"></div>
             </div>
-            <div className="col-4"></div>
-          </div>
           ) : (
             <div className="row border-top">
-            <div className="col-8 mt-2 ">
-              <p>{task.length} Items on the list</p>
+              <div className="col-8 mt-2 ">
+                <p>{task.length} Items on the list</p>
+              </div>
+              <div className="col-4">
+                <div
+                  className="btn btn-warning"
+                  onClick={() => handleDeleteAllTasks()}
+                >
+                  {" "}
+                  Delete All Tasks
+                </div>
+              </div>
             </div>
-            <div className="col-4"></div>
-          </div>
-          ) }
+          )}
         </div>
-          <div className="row mt-0 rounded shadow" style={style1} id="paginas">
-          </div>
-          <div className="row mt-0 rounded shadow" style={style1} id="paginas1">
-          </div>
+        <div
+          className="row mt-0 rounded shadow"
+          style={style1}
+          id="paginas"
+        ></div>
+        <div
+          className="row mt-0 rounded shadow"
+          style={style1}
+          id="paginas1"
+        ></div>
       </div>
     </div>
   );
